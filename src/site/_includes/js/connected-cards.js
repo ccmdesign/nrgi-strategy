54de656a45c4df171c3b136588abb14f9d5ae8f3
Maurício Mendonça <mauricio.azevedo@basis.com.br>
2020-09-03 02:56:08 -0300

    Add hover to connected cards

---
+++ src/site/_includes/js/connected-cards.js
@@ -0,0 +1,21 @@
+// Function to light up the paths when the cards are hovered
+function onCardsHover(elements) {
+    const cards = document.querySelectorAll(elements);
+
+    cards.forEach((card) => {
+        card.addEventListener("mouseover", function(event){ toggleLinesOpacity(event.target, true) });
+        card.addEventListener("mouseout", function(event){ toggleLinesOpacity(event.target, false) });
+    })
+}
+
+function toggleLinesOpacity(card, hover) {
+    let lines
+
+    card.id.includes('top-right') || card.id.includes('bottom-right')
+        ? lines = document.querySelectorAll('.line-bottom-left, .line-top-right, .line-right')
+        : lines = document.querySelectorAll('.line-top-left, .line-bottom-right, .line-left');
+
+    hover
+        ? lines.forEach((line) => {line.style.opacity = '1'})
+        : lines.forEach((line) => {line.style.opacity = '.1'})
+}
