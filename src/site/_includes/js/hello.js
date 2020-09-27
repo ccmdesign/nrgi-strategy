4543ae701691a216bc557e196f307ac59a6462db
Maurício Mendonça <mauricioms.azevedo@gmail.com>
2020-09-27 18:12:10 -0300

    New Modals

--- src/site/_includes/js/hello.js
+++ src/site/_includes/js/hello.js
@@ -15,24 +15,44 @@
 // menuItems('[data-inclusive-menu-opens]');
 
 // Get the dialog element (with the accessor method you want)
-const el1 = document.getElementById('card-1');
+const el1 = document.getElementById('modal-card-1');
 // Instantiate a new A11yDialog module
 const dialog1 = new A11yDialog(el1);
 
 // Get the dialog element (with the accessor method you want)
-const el2 = document.getElementById('card-2');
+const el2 = document.getElementById('modal-card-2');
 // Instantiate a new A11yDialog module
 const dialog2 = new A11yDialog(el2);
 // Get the dialog element (with the accessor method you want)
 
-const el3 = document.getElementById('card-3');
+const el3 = document.getElementById('modal-card-3');
 // Instantiate a new A11yDialog module
 const dialog3 = new A11yDialog(el3);
 // Get the dialog element (with the accessor method you want)
 
-const el4 = document.getElementById('card-4');
+const el4 = document.getElementById('modal-card-4');
 // Instantiate a new A11yDialog module
 const dialog4 = new A11yDialog(el4);
+
+// Get the dialog element (with the accessor method you want)
+const el5 = document.getElementById('modal-card-5');
+// Instantiate a new A11yDialog module
+const dialog5 = new A11yDialog(el5);
+
+// Get the dialog element (with the accessor method you want)
+const el6 = document.getElementById('modal-card-6');
+// Instantiate a new A11yDialog module
+const dialog6 = new A11yDialog(el6);
+// Get the dialog element (with the accessor method you want)
+
+const el7 = document.getElementById('modal-card-7');
+// Instantiate a new A11yDialog module
+const dialog7 = new A11yDialog(el7);
+// Get the dialog element (with the accessor method you want)
+
+const el8 = document.getElementById('modal-card-8');
+// Instantiate a new A11yDialog module
+const dialog8 = new A11yDialog(el8);
 
 // Add hover listener to connected cards on init
 addHoverListener('.pulse')
